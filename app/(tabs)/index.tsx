5bc6edf7e3cec2c700e8b24ee66f0e7a490ef483
enzosln <github@enzo-salson.fr>
2025-02-18 10:05:47 +0100

    :rocket: Update

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,16 @@
 import {Text} from "react-native";
+import {useAuth} from "@/hooks/useAuth";
+import {useEffect} from "react";
 
 export default function Tab() {
+
+  const {checkIsLogged} = useAuth();
+  useEffect(() => {
+    const check = async () => {
+      await checkIsLogged();
+    }
+    check().then(null);
+  }, []);
 
   return (
       <Text>salut</Text>
