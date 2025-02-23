90de2c666b42509270056330399f314cb778c4db
piguerry <pjguerry976@hotmail.fr>
2025-02-23 10:35:55 +0100

    proposition palettes

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,5 +1,6 @@
 export const theme = {
   colors: {
+    // DOCTOLIB
     backgroundPrimary: "#34659A",
     backgroundSecondary: "#DFF3FF",
     backgroundTertiary: "#FFFFFF",
@@ -7,6 +8,42 @@ export const theme = {
     textSecondary: "#333333",
     textTertiary: "",
     textEmphasis: "#4B94D8",
+
+    // Palette 1
+    // backgroundPrimary: "#2A7F62",
+    // backgroundSecondary: "#E3F6F5",
+    // backgroundTertiary: "#FFFFFF",
+    // textPrimary: "#000000",
+    // textSecondary: "#2C3E50",
+    // textTertiary: "",
+    // textEmphasis: "#1E6F5C",
+
+    // Palette 2
+    // backgroundPrimary: "#6A0DAD",
+    // backgroundSecondary: "#E6CCFF",
+    // backgroundTertiary: "#FFFFFF",
+    // textPrimary: "#000000",
+    // textSecondary: "#4B0082",
+    // textTertiary: "",
+    // textEmphasis: "#9A4EAE",
+
+    // Palette 3
+    // backgroundPrimary: "#D87C1D",
+    // backgroundSecondary: "#FFE8C8",
+    // backgroundTertiary: "#FFFFFF",
+    // textPrimary: "#000000",
+    // textSecondary: "#5A3E2B",
+    // textTertiary: "",
+    // textEmphasis: "#BF5700",
+
+    // Palette 4
+    // backgroundPrimary: "#A67B5B",
+    // backgroundSecondary: "#F2E0C9",
+    // backgroundTertiary: "#FFFFFF",
+    // textPrimary: "#000000",
+    // textSecondary: "#4E342E",
+    // textTertiary: "",
+    // textEmphasis: "#D4A276",
   },
   fontSizes: {
     small: 16,
